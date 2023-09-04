dc189ae874ff2a2d74862a003d35230fac3628e2
Italo <italo.vntonio@gmail.com>
2023-09-04 14:03:13 -0300

    Titulo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>oooooooo</Text>
+      <Text>TeloCambio</Text>
       <StatusBar style="auto" />
     </View>
   );
