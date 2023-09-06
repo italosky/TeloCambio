57ea37dfa19e50b8e16384ed9a2478af00df579f
Italo <italo.vntonio@gmail.com>
2023-09-05 22:55:37 -0300

    Revert "cambios"
    This reverts commit e486ad7e1ae2a8d6b97b937417a99840787afd6d.

--- Screens/Ingreso.js
+++ Screens/Ingreso.js
@@ -1,17 +1,14 @@
 import React, {Component} from 'react'
 import { Text, StyleSheet, View, Image, TextInput, TouchableOpacity, Alert, KeyboardAvoidingView, Platform } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
-import * as Google from 'expo-auth-session/providers/google';
-import firebase from 'firebase/app';
-import 'firebase/auth';
 
 export default function Ingreso(){
     const navigation = useNavigation();
 
     React.useLayoutEffect(() => {
     navigation.setOptions({
-      headerLeft: () => null,
-      gestureEnabled: false, 
+      headerLeft: () => null, // Esto oculta el botón para devolverse
+      gestureEnabled: false, // Esto deshabilita devolverse con el dedo
     });
     }, [navigation]);
     
@@ -21,24 +18,7 @@ export default function Ingreso(){
     const goRegistro=()=>{
         navigation.navigate('Registro')
     }
-    const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-        clientId: '231713560463-0u7k5s0udlfn8qrl6h0lf544g7poeaip.apps.googleusercontent.com',
-        });
-        
-        const signInWithGoogle = async () => {
-        try {
-            const result = await promptAsync();
-            if (result.type === 'success') {
-            const { id_token } = result.params;
-        
-            const googleAuth = firebase.auth;
-            const credential = googleAuth.GoogleAuthProvider.credential(id_token);
-            await googleAuth().signInWithCredential(credential);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    
     return (
         <View style={styles.padre}>
             <View style={styles.padreBoton}>
@@ -54,15 +34,15 @@ export default function Ingreso(){
                 </View>
 
                 <View style={styles.padreBoton}>
-                    <TouchableOpacity style={styles.cajaBotonG} onPress={signInWithGoogle}>
-                         <Text style={styles.textoBoton}>Iniciar con Google</Text>
+                    <TouchableOpacity style={styles.cajaBotonG}>
+                        <Text style={styles.textoBoton}>BAYRON CAMBIALO</Text>
                     </TouchableOpacity>
                 </View>
                 
             </View>
     )
 }
-
+//Estilos para los botones y texto
 const styles = StyleSheet.create({
     padre: {
         flex: 1,
