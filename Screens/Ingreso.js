632a59035e2710509ddc75650685120682970e2f
CarlaHuichaman <81341863+CarlaHuichaman@users.noreply.github.com>
2023-09-09 00:31:54 -0300

    Update from master (#1)
    * Merge pull request #2 from italosky/master
    
    cambios
    
    * Volviendo atras
    
    * indentacion xd
    
    * Auto format (#5)
    
    Co-authored-by: Italo <81238904+italosky@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Italo <81238904+italosky@users.noreply.github.com>
    Co-authored-by: BayronReyes <81239036+BayronReyes@users.noreply.github.com>
    Co-authored-by: Italo <italo.vntonio@gmail.com>
    Co-authored-by: Rodrigo Pizarro <r.antonio.regueiro@gmail.com>

--- Screens/Ingreso.js
+++ Screens/Ingreso.js
@@ -1,81 +1,93 @@
-import React, {Component} from 'react'
-import { Text, StyleSheet, View, Image, TextInput, TouchableOpacity, Alert, KeyboardAvoidingView, Platform } from 'react-native'
-import { useNavigation } from '@react-navigation/native';
+import React, { Component } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Ingreso(){
-    const navigation = useNavigation();
 
-    React.useLayoutEffect(() => {
+export default function Ingreso() {
+  const navigation = useNavigation();
+
+  React.useLayoutEffect(() => {
+
     navigation.setOptions({
       headerLeft: () => null, // Esto oculta el botón para devolverse
       gestureEnabled: false, // Esto deshabilita devolverse con el dedo
     });
-    }, [navigation]);
-    
-    const goLogin=()=>{
-        navigation.navigate('Login')
-    }
-    const goRegistro=()=>{
-        navigation.navigate('Registro')
-    }
-    
-    return (
-        <View style={styles.padre}>
-            <View style={styles.padreBoton}>
-                    <TouchableOpacity style={styles.cajaBotonL} onPress={goLogin}>
-                        <Text style={styles.textoBoton}>Iniciar Sesión</Text>
-                    </TouchableOpacity>
-                </View>
+  }, [navigation]);
 
-                <View style={styles.padreBoton}>
-                    <TouchableOpacity style={styles.cajaBotonR} onPress={goRegistro}>
-                        <Text style={styles.textoBoton}>Registrarse</Text>
-                    </TouchableOpacity>
-                </View>
+  const goLogin = () => {
+    navigation.navigate("Login");
+  };
+  const goRegistro = () => {
+    navigation.navigate("Registro");
+  };
 
-                <View style={styles.padreBoton}>
-                    <TouchableOpacity style={styles.cajaBotonG}>
-                        <Text style={styles.textoBoton}>BAYRON CAMBIALO</Text>
-                    </TouchableOpacity>
-                </View>
-                
-            </View>
-    )
+  return (
+    <View style={styles.padre}>
+      <View style={styles.padreBoton}>
+        <TouchableOpacity style={styles.cajaBotonL} onPress={goLogin}>
+          <Text style={styles.textoBoton}>Iniciar Sesión</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.padreBoton}>
+        <TouchableOpacity style={styles.cajaBotonR} onPress={goRegistro}>
+          <Text style={styles.textoBoton}>Registrarse</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.padreBoton}>
+        <TouchableOpacity style={styles.cajaBotonG}>
+          <Text style={styles.textoBoton}>BAYRON CAMBIALO</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+
 }
 //Estilos para los botones y texto
 const styles = StyleSheet.create({
-    padre: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white'
-    },
-    padreBoton:{
-        alignItems:'center',
-    },
-    cajaBotonL:{
-        backgroundColor:'#8AAD34',
-        borderRadius:'30',
-        paddingVertical:25,
-        width:300,
-        marginTop:35
-    },
-    cajaBotonR:{
-        backgroundColor:'#8AAD34',
-        borderRadius:'30',
-        paddingVertical:25,
-        width:300,
-        marginTop:35
-    },
-    cajaBotonG:{
-        backgroundColor:'#000',
-        borderRadius:'30',
-        paddingVertical:25,
-        width:300,
-        marginTop:35
-    },
-    textoBoton:{
-        textAlign:'center',
-        color:'white'
-    }
-})
+  padre: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  padreBoton: {
+    alignItems: "center",
+  },
+  cajaBotonL: {
+    backgroundColor: "#8AAD34",
+    borderRadius: "30",
+    paddingVertical: 25,
+    width: 300,
+    marginTop: 35,
+  },
+  cajaBotonR: {
+    backgroundColor: "#8AAD34",
+    borderRadius: "30",
+    paddingVertical: 25,
+    width: 300,
+    marginTop: 35,
+  },
+  cajaBotonG: {
+    backgroundColor: "#000",
+    borderRadius: "30",
+    paddingVertical: 25,
+    width: 300,
+    marginTop: 35,
+  },
+  textoBoton: {
+    textAlign: "center",
+    color: "white",
+  },
+});
