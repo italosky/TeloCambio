803ce7f6f73e97343559b089ec3688861368ab0c
Tomas Araya <helmo.1997@gmail.com>
2023-09-14 20:45:59 -0300

    CreacionPaginasFaltantes

---
+++ Screens/DetalleGratis.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import { Text, StyleSheet, View, Image, TextInput, TouchableOpacity, Alert, KeyboardAvoidingView, Platform } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+
+export default function Registro(){
+    return (
+        <View>
+            <Text>Hola Mundo Guapo</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
