c49196a2ce4b0c19aabf7fe8649e35d34d465fec
CarlaHuichaman <carlihuicha@gmail.com>
2023-09-16 04:33:09 -0300

    UI Galeria

---
+++ Screens/common/Articulos.js
@@ -0,0 +1,114 @@
+export const products = {
+  category: [
+    {
+      category: 'Accesorios',
+      data: [
+        {
+          nombre: 'Lentes de Sol',
+          estado: 'Nuevo',
+          imagen: require('/assets/Lentes.png'),
+        },
+        {
+          nombre: 'Collares Fantasía',
+          estado: 'Nuevo',
+          imagen: require('/assets/Collares.png'),
+        },
+        {
+          nombre: 'Pinches Mariposa',
+          estado: 'Nuevo',
+          imagen: require('/assets/Pinches.png'),
+        },
+      ],
+    },
+    {
+      category: 'Comida',
+      data: [
+        {
+          nombre: 'Lentejas',
+          estado: 'Nuevo',
+          imagen: require('/assets/Lentejas.png'),
+        },
+        {
+          nombre: 'Galletas',
+          estado: 'Nuevo',
+          imagen: require('/assets/Galletas.png'),
+        },
+        {
+          nombre: 'Jugos TetraPack',
+          estado: 'Nuevo',
+          imagen: require('/assets/Jugos.png'),
+        },
+      ],
+    },
+    {
+      category: 'Deportes',
+      data: [
+        {
+          nombre: 'Casco Moto',
+          estado: 'Nuevo',
+          imagen: require('/assets/Casco.png'),
+        },
+        {
+          nombre: 'Patines Oxelo',
+          estado: 'Semi-Nuevo',
+          imagen: require('/assets/Patines.png'),
+        },
+        {
+          nombre: 'Luces de Bicicleta',
+          estado: 'Usadas',
+          imagen: require('/assets/LucesBici.png'),
+        },
+      ],
+    },
+    {
+      category: 'Ferretería',
+      data: [
+        {
+          nombre: 'Frague',
+          estado: 'Nuevo',
+          imagen: require('/assets/Casco.png'),
+        },
+      ],
+    },
+    {
+      category: 'Hogar',
+      data: [
+        {
+          nombre: 'Lampara',
+          estado: 'Usado',
+          imagen: require('/assets/Casco.png'),
+        },
+      ],
+    },
+    {
+      category: 'Instrumentos',
+      data: [
+        {
+          nombre: 'Guitarra',
+          estado: 'Usado',
+          imagen: require('/assets/Casco.png'),
+        },
+      ],
+    },
+    {
+      category: 'Juguetes',
+      data: [
+        {
+          nombre: 'Barbie Cocinera',
+          estado: 'Usado',
+          imagen: require('/assets/Casco.png'),
+        },
+      ],
+    },
+    {
+      category: 'Libros',
+      data: [
+        {
+          nombre: 'After',
+          estado: 'Usada',
+          imagen: require('/assets/Casco.png'),
+        },
+      ],
+    },
+  ],
+}      
---
+++ Screens/common/MyProductItem.js
@@ -0,0 +1,66 @@
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { Card } from 'react-native-paper';
+import React from 'react';
+
+const MyProductItem = ({item}) => {
+  return (
+    <Card style={styles.containerCard}>
+      <Card.Cover source={item.imagen} style={styles.imagen}/>
+      <Text style={styles.titleCard}>{item.nombre}</Text>
+      <View style={styles.viewCard}>
+        <Text style={styles.textCard}>{item.estado}</Text>
+        <TouchableOpacity style={styles.buttonCard}>
+          <Text style={styles.textButton}>Solicitar</Text>
+        </TouchableOpacity> 
+      </View>
+    </Card>
+  );
+};
+
+export default MyProductItem;
+
+const styles = StyleSheet.create({
+  containerCard: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  imagen: {
+    width: '100%',
+    height: 130,
+    borderRadius: 5,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  titleCard: {
+    marginLeft: 10,
+    marginTop: 5,
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  viewCard: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 4,
+    alignItems: 'center',
+  },
+  textCard: {
+    fontSize: 14,
+    fontWeight: 500,
+  },
+  buttonCard: {
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    padding: 5,
+    backgroundColor: "#8AAD34",
+  },
+  textButton: {
+    color: "#ffffff",
+  },
+});
