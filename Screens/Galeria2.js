043260dcb545eeb1a87f949823b45142ba40ae0b
Tomas Araya <helmo.1997@gmail.com>
2023-10-04 06:34:14 -0300

    Add: Show Gallery Articles

--- Screens/Galeria2.js
+++ Screens/Galeria2.js
@@ -3,7 +3,7 @@ import {
   StyleSheet,
   Text,
   View,
-  Image,
+  Image, 
   TouchableOpacity,
   ScrollView,
   Platform,
@@ -15,23 +15,66 @@ import { FlatList } from "react-native-gesture-handler";
 import { Drawer, AnimatedFAB } from "react-native-paper";
 import { auth } from "../firebaseConfig";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
 
 export default function Home() {
-    const [isExtended, setIsExtended] = React.useState(true);
-
-    const onScroll = ({ nativeEvent }) => {
-      const currentScrollPosition =
-        Math.floor(nativeEvent?.contentOffset?.y) ?? 0;
-  
-      setIsExtended(currentScrollPosition <= 0);
-    };
-    React.useLayoutEffect(() => {
-        navigation.setOptions({
-          headerLeft: () => null,
-          gestureEnabled: false,
-        });
-      }, [navigation]);   
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isExtended, setIsExtended] = useState(true);
   const navigation = useNavigation();
+  const [refreshing, setRefreshing] = useState(false);
+  const [numColumns, setNumColumns] = useState(2);
+  const drawer = useRef(null);
+  const [drawerPosition, setDrawerPosition] = useState("left");
+
+  const fetchPosts = async () => {
+    try {
+      setLoading(true);
+      const allItemsArray = [];
+      const articulosPublicadosRef = collection(db, 'Publicaciones');
+      const usersSnapshot = await getDocs(articulosPublicadosRef);
+      usersSnapshot.forEach((postDoc) => {
+        const postData = postDoc.data();
+        allItemsArray.push({
+          id: postDoc.id,
+          src: postData.imagenURL,
+          nombreArticulo: postData.nombreArticulo,
+          tipo: postData.tipo,
+          estadoArticulo: postData.estadoArticulo,
+          comuna: postData.comuna,
+        });
+      });
+      setDataSource(allItemsArray);
+    } catch (error) {
+      console.error("Error al cargar los artículos:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const handleRefresh = async () => {
+    setRefreshing(true);
+    await fetchPosts();
+    setRefreshing(false);
+  };
+
+  const onScroll = ({ nativeEvent }) => {
+    const currentScrollPosition =
+      Math.floor(nativeEvent?.contentOffset?.y) ?? 0;
+    setIsExtended(currentScrollPosition <= 0);
+  };
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => null,
+      gestureEnabled: false,
+    });
+  }, [navigation]);
 
   const cerrarSesion = async () => {
     try {
@@ -42,16 +85,6 @@ export default function Home() {
       console.error("Error al cerrar sesión:", error);
     }
   };
-
-  const [numColumns, setNumColumns] = useState(2);
-  const [dataSource, setDataSource] = useState([]);
-
-  useEffect(() => {
-    let items = Array.apply(null, Array(60)).map((v, i) => {
-      return { id: i, src: 'http://placehold.it/200x200?text=' + (i + 1) };
-    });
-    setDataSource(items);
-  }, []);
 
   const goMiPerfil = () => {
     navigation.navigate("MiPerfil");
@@ -73,9 +106,6 @@ export default function Home() {
     navigation.navigate("SubirArticulos");
   };
 
-  const drawer = useRef(null);
-  const [drawerPosition, setDrawerPosition] = useState("left");
-
   const changeDrawerPosition = () => {
     if (drawerPosition === "left") {
       setDrawerPosition("right");
@@ -92,18 +122,24 @@ export default function Home() {
       renderNavigationView={navigationView}
     >
       <View style={styles.container}>
+      {loading ? (
+        <Text>Cargando...</Text>
+      ) : (
         <FlatList
-          data={dataSource}
+          data={dataSource}  
           renderItem={renderItem}
           numColumns={numColumns}
-          keyExtractor={(item, index) => index.toString()}
+          keyExtractor={(item) => item.id.toString()}
           contentContainerStyle={styles.gridContainer}
+          onRefresh={handleRefresh}
+          refreshing={refreshing}
         />
+      )}
       </View>
       <AnimatedFAB
         icon="plus"
         label="Subir Artículo"
-        onPress={goSubirArticulos}
+        onPress={() => navigation.navigate("SubirArticulos")}
         style={styles.fabStyle}
         extended={isExtended}
         visible={true}
@@ -113,7 +149,7 @@ export default function Home() {
       />
     </DrawerLayout>
   );
-
+   
   const navigationView = () => (
     <View style={[styles.containerDrawer, styles.navigationContainer]}>
       <View>
@@ -122,22 +158,18 @@ export default function Home() {
           style={styles.logo}
         />
       </View>
-
       <View style={styles.separatorLine} />
 
       <Drawer.Section>
         <TouchableOpacity style={styles.drawerItem} onPress={goMiPerfil}>
           <Text style={styles.drawerText}>Mi Perfil</Text>
         </TouchableOpacity>
-
         <TouchableOpacity style={styles.drawerItem} onPress={goGaleria2}>
           <Text style={styles.drawerText}>Galería de Artículos</Text>
         </TouchableOpacity>
-
         <TouchableOpacity style={styles.drawerItem} onPress={goMisPublicados}>
           <Text style={styles.drawerText}>Mis Publicados</Text>
         </TouchableOpacity>
-
         <TouchableOpacity style={styles.drawerItem} onPress={goMisOfertas}>
           <Text style={styles.drawerText}>Mis Ofertas</Text>
         </TouchableOpacity>
@@ -155,6 +187,11 @@ export default function Home() {
   const renderItem = ({ item }) => (
     <View style={styles.itemContainer}>
       <Image style={styles.imageThumbnail} source={{ uri: item.src }} />
+      <View style={styles.itemOverlay}>
+        <Text style={styles.itemName}>{item.nombreArticulo || ''}</Text>
+        <Text style={styles.itemInfo}>{item.tipo || ''}</Text> 
+        <Text style={styles.itemInfo}>{item.comuna || ''}</Text>
+      </View>
     </View>
   );
 
@@ -215,11 +252,29 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     margin: 2,
+    position: 'relative', 
   },
   imageThumbnail: {
     justifyContent: 'center',
     alignItems: 'center',
     height: 200,
-    
+    width: '100%', 
+    resizeMode: 'cover',
+  },
+  itemOverlay: {
+    position: 'absolute', 
+    bottom: 0, 
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    width: '100%', 
+    padding: 10,
+  },
+  itemName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#FFF', 
+  },
+  itemInfo: {
+    fontSize: 14,
+    color: '#FFF', 
   },
 });
