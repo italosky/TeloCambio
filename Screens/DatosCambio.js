7c1a80cbc9cc221dc804bc6235fa442a46593701
Tomas Araya <helmo.1997@gmail.com>
2023-10-23 00:30:29 -0300

    Fix: Correo

--- Screens/DatosCambio.js
+++ Screens/DatosCambio.js
@@ -69,12 +69,11 @@ export default function DatosCambio({ route }) {
       const email = userData.email;
       const nombreArticulo = item.nombreArticulo;
       const nombrePersona = userData.nombre_apellido;
-      const subject = `Solicitud Articulo ${nombreArticulo} `;
-      const message = `Hola ${nombrePersona}, deseo reclamar tu articulo publicado en la aplicación TeloCambio🌱 *${nombreArticulo}* Me gustaría que conversáramos para coordinar la entrega. 
-      `;
-      const gmailURL = `googlegmail://co?to=${email}&subject=${encodeURIComponent(
-        subject
-      )}&body=${encodeURIComponent(message)}`;
+      const subject = `Solicitud Articulo ${nombreArticulo}`;
+      const message = `Hola ${nombrePersona}, deseo reclamar tu artículo publicado en la aplicación TeloCambio🌱 ${nombreArticulo}. Me gustaría que conversáramos para coordinar la entrega.`;
+  
+      const gmailURL = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+  
       Linking.openURL(gmailURL)
         .then(() => {
           console.log("Abriendo Gmail con asunto y mensaje predeterminados...");
